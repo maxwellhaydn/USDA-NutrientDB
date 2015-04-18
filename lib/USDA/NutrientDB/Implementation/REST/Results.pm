27948d1e0828b2d6c96218fa4095194dfa52eb57
Maxwell H. Carey <maxwellhaydn@gmail.com>
2015-04-17 23:03:35 -0600

    Implemented next() method
    Implemented the next() method to iterate through results, caching them as it
    builds them.

--- lib/USDA/NutrientDB/Implementation/REST/Results.pm
+++ lib/USDA/NutrientDB/Implementation/REST/Results.pm
@@ -19,15 +19,15 @@ has 'keyword' => (
     required => 1
 );
 
-has '_matching_items' => (
+has '_search_data' => (
     is       => 'ro',
     isa      => 'Arrayref',
     init_arg => undef,
     lazy     => 1,
-    builder  => '_get_matching_items'
+    builder  => '_get_search_data'
 );
 
-has '_results' => (
+has '_cache' => (
     is       => 'ro',
     isa      => 'Arrayref',
     init_arg => undef
@@ -39,58 +39,75 @@ sub next {
     my $self = shift;
 
     # Return next cached FoodItem, if there is one
-#    return shift $self->_results if @{ $self->_results };
-#
-#    # Otherwise, fetch nutrient details for next search result and re-populate
-#    # the cache
-#    my $next = shift $self->_matching_items;
-#
-#    my @nutrients = $self->_get_nutrients($next->ndbno);
-#
-#    # Get unique measures (e.g. "cup, diced", "oz", "cubic inch") for this item
-#    my %measures;
-#    #foreach my $nutrient (@nutrients) {
-#    #    @measures{  } = map { $_->{label} } @{ $nutrient->{measures} };
-#
-#    foreach my $nutrient (@nutrients) {
-#        my $item = USDA::NutrientDB::FoodItem->new(
-#            ndbno => $next->ndbo,
-#            name  => $next->name,
-#            food_group => $next->food_group,
-#            #quantity => $nutrient->
-#        );
-#    }
+    return shift @{ $self->_cache } if $self->_cache;
 
-    return USDA::NutrientDB::FoodItem->new(
-        food_group => 'foo',
-        name       => 'bar',
-        ndbno      => '123456789'
-    );
+    # Otherwise, fetch nutrient details for next search result and re-populate
+    # the cache
+    my $next_match = shift $self->_search_data;
+
+    my @nutrient_data = $self->_get_nutrient_data($next_match->{ndbno});
+
+    # Generate a new FoodItem for each unique measure (e.g. "cup, diced", "oz",
+    # "cubic inch") returned in the nutrient report
+    my %items;
+    foreach my $nutrient (@nutrient_data) {
+        my $nutrient_object = USDA::NutrientDB::Nutrient->new(
+            name => $nutrient->{name},
+            unit => $nutrient->{unit}
+        );
+
+        foreach my $measure ($nutrient->{measures}) {
+
+            # Amount of the nutrient, e.g. 48.97 g of water, 536.0 kcal of
+            # energy
+            $nutrient_object->quantity($measure->{value});
+
+            if (exists $items{$measure}) {
+                $items{$measure}->addNutrient($nutrient_object);
+            }
+            else {
+                $items{$measure} = USDA::NutrientDB::FoodItem->new(
+                    ndbno      => $next_match->{ndbno},
+                    name       => $next_match->{name},
+                    food_group => $next_match->{fg},
+                    quantity   => $measure->{qty},
+                    unit       => $measure->{label},
+                    grams      => $measure->{eqv},
+                    nutrients  => [ $nutrient_object ]
+                );
+            }
+        }
+    }
+
+    # Re-populate the cache and return the first FoodItem in the list
+    $self->_cache(values %items);
+
+    return shift $self->_cache;
 }
 
 # Submit a search request to the REST API. This returns a list of matching food
 # items' names, ndbno values, and food groups, but no other details.
-sub _get_matching_items {
+sub _get_search_data {
     my $self = shift;
 
     my $query_string = REST::Client::buildQuery(q => $self->keyword);
     my $url = '/usda/ndb/search/' . $query_string;
 
-    $self->rest_client->GET($url);
+    $self->_rest_client->GET($url);
 
     # TODO: Handle errors
 
-    if ($self->rest_client->responseCode eq '200') {
-        my $results = decode_json($self->rest_client->responseContent);
+    if ($self->_rest_client->responseCode eq '200') {
+        my $results = decode_json($self->_rest_client->responseContent);
         return $results->{list}{item};
     }
 
     return;
 }
 
-# Submit a request for a detailed food report for the given item and return a
-# list of nutrients.
-sub _get_nutrients {
+# Submit a request for a detailed food report for the given item and return the
+# associated nutrient data.
+sub _get_nutrient_data {
     my $self = shift;
     my $ndbno = shift;
 
@@ -100,12 +117,12 @@ sub _get_nutrients {
     );
     my $url = '/usda/ndb/search/' . $query_string;
 
-    $self->rest_client->GET($url);
+    $self->_rest_client->GET($url);
 
     # TODO: Handle errors
 
-    if ($self->rest_client->responseCode eq '200') {
-        my $results = decode_json($self->rest_client->responseContent);
+    if ($self->_rest_client->responseCode eq '200') {
+        my $results = decode_json($self->_rest_client->responseContent);
         return $results->{list}{item};
     }
 
